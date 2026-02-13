3ff88bbab1a32a4fd0cf6c37e3b1f25d59c13d12
unknown <abdulatifsh90@gmail.com>
2026-02-14 02:27:38 +0900

    BRR: create components for auth

---
+++ components/ui/auth/social.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { Button } from "../button";
+
+export const Social = () => {
+    return (
+        <div className="flex items-center w-full gap-x-2">
+            <Button
+                size="lg"
+                variant="outline"
+                className="flex-1 w-full"
+                onClick={() => {}}>
+                <FcGoogle className="h-5 w-5"/>
+            </Button>
+            <Button
+                size="lg"
+                variant="outline"
+                className="flex-1 w-full"
+                onClick={() => {}}>
+                <FaGithub className="h-5 w-5"/>
+            </Button>        
+        </div>
+    )
+};
